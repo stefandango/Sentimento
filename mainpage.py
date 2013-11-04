80f1259de233c79616d0a3e281ecb1b3b00b3478
Stefan <stefanoep@gmail.com>
2013-11-04 15:38:37 +0100

    Added post functionality

--- mainpage.py
+++ mainpage.py
@@ -1,6 +1,7 @@
 import webapp2
 import jinja2
-
+import os
+import cgi
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -11,11 +12,21 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-		template = JINJA_ENVIRONMENT.get_template('index.html')
+		template_values = {}
+		template = JINJA_ENVIRONMENT.get_template('scrape.html')
 		self.response.write(template.render(template_values))
 
+class ShowResults(webapp2.RequestHandler):
+
+	def post(self):
+		topic = (cgi.escape(self.request.get('Topic')))
+
+		template_values = {"Topic": topic}
+		template = JINJA_ENVIRONMENT.get_template('results.html')
+		self.response.write(template.render(template_values))
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/result', ShowResults),
 ], debug=True)
 
