af09e1e301c89a314bb5f437daa7edda9a3615eb
Stefan <stefanoep@gmail.com>
2013-11-04 14:43:53 +0100

    Added MORE basic files

--- mainpage.py
+++ mainpage.py
@@ -2,13 +2,20 @@ import webapp2
 import jinja2
 
 
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+		template = JINJA_ENVIRONMENT.get_template('index.html')
+		self.response.write(template.render(template_values))
 
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
 ], debug=True)
+
