a383900c69e30eaef0f3093d617d1d26b79e3b70
unknown <kennykudda@gmail.com>
2013-12-05 11:00:24 +0100

    FreqDist files added

---
+++ src/freqdist.py
@@ -0,0 +1,48 @@
+import nltk
+
+#	Call:
+#		fd = FrequencyDistribution(contents, 4)   
+#		print fd.distribution.items()[:20];
+#
+
+class FrequencyDistribution:
+
+	def __init__(self, contents, threshold):
+		self.threshold = threshold
+		self.distribution = self.__ComputeFrequencyDistribution(contents)
+		
+	def Print(self):
+		print self.distribution.items()[:20]
+
+	def __ComputeFrequencyDistribution(self, contents):
+		#remove bad characters
+		contents = self.__RemoveBadChars(contents)
+		#tokenize
+		tokenized_words = nltk.tokenize.word_tokenize(contents)
+		#remove Stopwords
+		removed_stopwords = self.__RemoveStopwords(tokenized_words)
+		#remove words below some length-threshold
+		thresholded = self.__RemoveThreshold(removed_stopwords, self.threshold)
+		return nltk.FreqDist(thresholded)
+	
+	def __RemoveBadChars(self, text):
+		badchars = open('badchars.data').read().splitlines()
+		for badchar in badchars:
+			text = text.replace(badchar, "")
+		return text
+	
+	def __RemoveStopwords(self, wordlist):
+		danishstopwords = open('danishstopwords.data').read().splitlines()
+		return [ word for word in wordlist if word not in danishstopwords]
+	
+	def __RemoveThreshold(self, words, threshold):
+		return [word for word in words if len(word) >= threshold]
+
+if __name__ == "__main__":
+	if len(sys.argv) > 1:
+		path = sys.argv[1]
+		contents = open(path, 'r').read()
+		fd = FrequencyDistribution(contents, 4)   
+		print fd.distribution.items()[:20];
+	else:
+		print "Provide a path to text file."    
