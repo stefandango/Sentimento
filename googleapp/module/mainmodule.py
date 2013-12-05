4698a381db452ccceb21e50d97507b2fdbf1e265
Stefan <stefanoep@gmail.com>
2013-12-05 10:24:37 +0100

    fixed error

--- googleapp/module/mainmodule.py
+++ googleapp/module/mainmodule.py
@@ -8,10 +8,10 @@ import cProfile, pstats, StringIO
 import urlparse
 from articletext import Articlescrape
 from SentimentAnalysis import SentimentAnalysis
-from multiprocessing import Pool
+#from multiprocessing import Pool
 from collections import defaultdict
 import re
 import json
 
 
-configdict = json.load(open('json_data'))
+#configdict = json.load(open('scrape/scrapeconfigeration.json'))
