fb4a1ab036079f48785b01c1bc68a7f6e7be7419
unknown <kennykudda@gmail.com>
2013-12-05 11:27:42 +0100

    FreqDist updated again

--- src/freqdist.py
+++ src/freqdist.py
@@ -10,9 +10,16 @@ class FrequencyDistribution:
 	def __init__(self, contents, threshold):
 		self.threshold = threshold
 		self.distribution = self.__ComputeFrequencyDistribution(contents)
-		
+
 	def Print(self):
 		print self.distribution.items()[:20]
+
+	def ChartString(self):
+		output_str = str(self.distribution.items()[:20])
+		output_str = output_str.replace("(", "[")
+		output_str = output_str.replace(")", "]")
+		output_str = output_str.replace("[[", "[['Word', 'Occurrences'], [")
+		return output_str
 
 	def __ComputeFrequencyDistribution(self, contents):
 		#remove bad characters
@@ -24,16 +31,16 @@ class FrequencyDistribution:
 		#remove words below some length-threshold
 		thresholded = self.__RemoveThreshold(removed_stopwords, self.threshold)
 		return nltk.FreqDist(thresholded)
-	
+
 	def __RemoveBadChars(self, text):
 		badchars = open('badchars.data').read().splitlines()
 		for badchar in badchars:
 			text = text.replace(badchar, "")
 		return text
-	
+
 	def __RemoveStopwords(self, wordlist):
 		danishstopwords = open('danishstopwords.data').read().splitlines()
 		return [ word for word in wordlist if word not in danishstopwords]
-	
+
 	def __RemoveThreshold(self, words, threshold):
 		return [word for word in words if len(word) >= threshold]
