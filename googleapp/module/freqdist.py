084e0e853ec81b93b2ca867ad2b553dcb0990e83
unknown <kennykudda@gmail.com>
2013-12-06 12:19:47 +0100

    Updated FreqDist again

--- googleapp/module/freqdist.py
+++ googleapp/module/freqdist.py
@@ -6,16 +6,6 @@ class FrequencyDistribution:
 	def __init__(self, contents, threshold):
 		self.threshold = threshold
 		self.distribution = self.__ComputeFrequencyDistribution(contents)
-
-	def Print(self, entries):
-		print self.distribution.items()[:entries]
-
-	def ChartString(self, entries=20):
-		output_str = str(self.distribution.items()[:entries])
-		output_str = output_str.replace("(", "[")
-		output_str = output_str.replace(")", "]")
-		output_str = output_str.replace("[[", "[['Word', 'Occurrences'], [")
-		return output_str
 	
 	def __ComputeFrequencyDistribution(self, contents):
 		#remove bad characters
@@ -40,3 +30,13 @@ class FrequencyDistribution:
 
 	def __RemoveThreshold(self, words, threshold):
 		return [word for word in words if len(word) >= threshold]
+	
+	def Print(self, entries):
+		print self.distribution.items()[:entries]
+
+	def ChartString(self, entries=20):
+		output_str = str(self.distribution.items()[:entries])
+		output_str = output_str.replace("(", "[")
+		output_str = output_str.replace(")", "]")
+		output_str = output_str.replace("[[", "[['Word', 'Occurrences'], [")
+		return output_str
